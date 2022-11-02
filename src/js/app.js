5067e5c3d839b3b2e67502763a2ec7fc2e8954c6
Damjan83 <damjanpokrajac83@gmail.com>
2022-11-02 20:10:03 +0100

    changes

--- src/js/app.js
+++ src/js/app.js
@@ -39,59 +39,7 @@ $( document ).ready(function() {
     });
     
 });
-// window.addEventListener('load' , function() {
-//     const form = document.querySelector('#todo__form');
-//     const input = document.querySelector('#todo__input');
-//     const tasks = document.querySelector('.todo__tasks');
 
-//     form.addEventListener('submit' , function(e) {
-//         e.preventDefault();
-//         const task = input.value;
-//         if(!task) {
-//             alert('Please fill out the task');
-//             return;
-//         }
-
-//         const taskEle = document.createElement('div');
-//         taskEle.classList.add('task');
-
-//         const taskInput = document.createElement('input');
-//         taskInput.classList.add('text');
-//         taskInput.type = 'text';
-//         taskInput.value = task;
-//         taskInput.setAttribute('readonly' , 'readonly');
-//         taskEle.appendChild(taskInput);
-
-//         const btnEdit = document.createElement('button');
-//         btnEdit.classList.add('edit');
-//         btnEdit.innerHTML = 'edit';
-//         const btnDelete = document.createElement('button');
-//         btnDelete.classList.add('delete');
-//         btnDelete.innerHTML = 'delete';
-//         taskEle.appendChild(btnEdit)
-//         taskEle.appendChild(btnDelete)
-
-//         tasks.appendChild(taskEle);
-//         input.value = '';
-
-//         btnEdit.addEventListener('click' , function() {
-//             if(btnEdit.innerHTML.toLowerCase() == 'edit') {
-//                 taskInput.removeAttribute('readonly');
-//                 taskInput.focus();
-//                 btnEdit.innerHTML = 'save';
-//             } else {
-//                 taskInput.setAttribute('readonly' , 'readonly');
-//                 btnEdit.innerHTML = 'edit'               
-//             }     
-//         })
-
-//         btnDelete.addEventListener('click' , function() {
-//             taskEle.removeChild(taskInput)
-//             taskEle.removeChild(btnEdit)
-//             taskEle.removeChild(btnDelete)
-//         })
-//     })
-// })
 
 
 
